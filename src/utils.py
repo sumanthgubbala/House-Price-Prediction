5677bf4c4793eafbe15a51e69c4f98cf7e5499a4
sumanthgubbala <sumanthgubbala123@gmail.com>
2025-08-21 00:40:56 +0530

    model training

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 
 import pickle
 from src.exception import CustomException
+from sklearn.metrics import r2_score
+
 
 
 def save_object(file_path, obj):
@@ -15,3 +17,27 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(X_train,y_train)
+
+            y_train_pred = model.predict(X_train)
+            # Predict Testing data
+            y_test_pred =model.predict(X_test)
+
+            # Get R2 scores for train and test data
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
